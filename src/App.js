40369835a9d4b1f23d104705f8c5a523cf46109c
Odewole Kehinde <kennyedward99@gmail.com>
2020-09-23 17:46:11 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import { DatePicker, Button, version, message, Alert } from 'antd'
+import 'antd/dist/antd.css'
+import "./styles.css";
+
+export default function App() {
+  const [date, setDate] = useState('')
+  const handleChange = value => {
+    message.info(`Yay Date: ${value ? value.format('DD-MM-YYYY') : 'None'}`)
+    setDate(value)
+  }
+  return (
+    <div className="App">
+      <h1>Antd version {version}</h1>
+      <DatePicker onChange={handleChange} style={{borderRadius: 10}} />
+      <Button type='primary' style={{marginLeft: 8}} >
+        Select Date
+      </Button>
+      <Alert message="Selected Date" description={date ? date.format('DD-MM-YYYY') : 'None'} />
+    </div>
+  );
+}
